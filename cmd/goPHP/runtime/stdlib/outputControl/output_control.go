d271cb71798f5e884ce5b7ba890357d1e35f960b
MasterZydra <github@davidhein.de>
2025-07-26 19:45:17 +0200

    StdLib -  Output Control: Added notices

--- cmd/goPHP/runtime/stdlib/outputControl/output_control.go
+++ cmd/goPHP/runtime/stdlib/outputControl/output_control.go
@@ -10,12 +10,12 @@ import (
 func Register(environment runtime.Environment) {
 	// Category: Output Control Functions
 	environment.AddNativeFunction("ob_clean", nativeFn_ob_clean)
-	environment.AddNativeFunction("ob_flush", nativeFn_ob_flush)
 	environment.AddNativeFunction("ob_end_clean", nativeFn_ob_end_clean)
 	environment.AddNativeFunction("ob_end_flush", nativeFn_ob_end_flush)
+	environment.AddNativeFunction("ob_flush", nativeFn_ob_flush)
 	environment.AddNativeFunction("ob_get_clean", nativeFn_ob_get_clean)
-	environment.AddNativeFunction("ob_get_flush", nativeFn_ob_get_flush)
 	environment.AddNativeFunction("ob_get_contents", nativeFn_ob_get_contents)
+	environment.AddNativeFunction("ob_get_flush", nativeFn_ob_get_flush)
 	environment.AddNativeFunction("ob_get_level", nativeFn_ob_get_level)
 	environment.AddNativeFunction("ob_start", nativeFn_ob_start)
 }
@@ -35,44 +35,12 @@ func nativeFn_ob_clean(args []values.RuntimeValue, context runtime.Context) (val
 	// This function calls the output handler (with the PHP_OUTPUT_HANDLER_CLEAN flag),
 	// discards it's return value and cleans (erases) the contents of the active output buffer.
 
-	// TODO Throw notice if no buffer: e.g. Notice: ob_clean(): Failed to delete buffer. No buffer to delete in /home/user/scripts/code.php on line 5
-
 	if context.Interpreter.GetOutputBufferStack().Len() == 0 {
+		context.Interpreter.PrintError(phpError.NewNotice("ob_clean(): Failed to delete buffer. No buffer to delete in %s", context.Stmt.GetPosition().ToPosString()))
 		return values.NewBool(false), nil
 	}
 
 	context.Interpreter.GetOutputBufferStack().GetLast().Content = ""
-	return values.NewBool(true), nil
-}
-
-// -------------------------------------- ob_flush -------------------------------------- MARK: ob_flush
-
-func nativeFn_ob_flush(args []values.RuntimeValue, context runtime.Context) (values.RuntimeValue, phpError.Error) {
-	// Spec: https://www.php.net/manual/en/function.ob-flush.php
-
-	_, err := funcParamValidator.NewValidator("ob_flush").Validate(args)
-	if err != nil {
-		return values.NewVoid(), err
-	}
-
-	// TODO Call output handler
-	// Spec: https://www.php.net/manual/en/function.ob-flush.php
-	// This function calls the output handler (with the PHP_OUTPUT_HANDLER_FLUSH flag),
-	// discards it's return value and flushs (erases) the contents of the active output buffer.
-
-	// TODO Throw notice if no buffer: e.g. Notice: ob_flush(): Failed to delete buffer. No buffer to delete in /home/user/scripts/code.php on line 5
-
-	if context.Interpreter.GetOutputBufferStack().Len() == 0 {
-		return values.NewBool(false), nil
-	}
-
-	if context.Interpreter.GetOutputBufferStack().Len() == 1 {
-		context.Interpreter.WriteResult(context.Interpreter.GetOutputBufferStack().GetLast().Content)
-	} else {
-		context.Interpreter.GetOutputBufferStack().Get(context.Interpreter.GetOutputBufferStack().Len() - 2).Content += context.Interpreter.GetOutputBufferStack().GetLast().Content
-	}
-
-	nativeFn_ob_clean(args, context)
 	return values.NewBool(true), nil
 }
 
@@ -91,9 +59,8 @@ func nativeFn_ob_end_clean(args []values.RuntimeValue, context runtime.Context) 
 	// This function calls the output handler (with the PHP_OUTPUT_HANDLER_CLEAN and PHP_OUTPUT_HANDLER_FINAL flags),
 	// discards it's return value, discards the contents of the active output buffer and turns off the active output buffer.
 
-	// TODO Throw notice if no buffer: e.g. Notice: ob_end_clean(): Failed to delete buffer. No buffer to delete in /home/user/scripts/code.php on line 5
-
 	if context.Interpreter.GetOutputBufferStack().Len() == 0 {
+		context.Interpreter.PrintError(phpError.NewNotice("ob_end_clean(): Failed to delete buffer. No buffer to delete in %s", context.Stmt.GetPosition().ToPosString()))
 		return values.NewBool(false), nil
 	}
 
@@ -120,14 +87,43 @@ func ObEndFlush(context runtime.Context) (values.RuntimeValue, phpError.Error) {
 	// This function calls the output handler (with the PHP_OUTPUT_HANDLER_FINAL flag),
 	// flushes (sends) it's return value, discards the contents of the active output buffer and turns off the active output buffer.
 
-	// TODO Throw notice if no buffer: e.g. Notice: ob_end_flush(): Failed to delete buffer. No buffer to delete in /home/user/scripts/code.php on line 5
-
 	if context.Interpreter.GetOutputBufferStack().Len() == 0 {
+		context.Interpreter.PrintError(phpError.NewNotice("ob_end_flush(): Failed to delete and flush buffer. No buffer to delete or flush in %s", context.Stmt.GetPosition().ToPosString()))
 		return values.NewBool(false), nil
 	}
 
 	nativeFn_ob_flush([]values.RuntimeValue{}, context)
 	context.Interpreter.GetOutputBufferStack().Pop()
+	return values.NewBool(true), nil
+}
+
+// -------------------------------------- ob_flush -------------------------------------- MARK: ob_flush
+
+func nativeFn_ob_flush(args []values.RuntimeValue, context runtime.Context) (values.RuntimeValue, phpError.Error) {
+	// Spec: https://www.php.net/manual/en/function.ob-flush.php
+
+	_, err := funcParamValidator.NewValidator("ob_flush").Validate(args)
+	if err != nil {
+		return values.NewVoid(), err
+	}
+
+	// TODO Call output handler
+	// Spec: https://www.php.net/manual/en/function.ob-flush.php
+	// This function calls the output handler (with the PHP_OUTPUT_HANDLER_FLUSH flag),
+	// discards it's return value and flushs (erases) the contents of the active output buffer.
+
+	if context.Interpreter.GetOutputBufferStack().Len() == 0 {
+		context.Interpreter.PrintError(phpError.NewNotice("ob_flush(): Failed to flush buffer. No buffer to flush in %s", context.Stmt.GetPosition().ToPosString()))
+		return values.NewBool(false), nil
+	}
+
+	if context.Interpreter.GetOutputBufferStack().Len() == 1 {
+		context.Interpreter.WriteResult(context.Interpreter.GetOutputBufferStack().GetLast().Content)
+	} else {
+		context.Interpreter.GetOutputBufferStack().Get(context.Interpreter.GetOutputBufferStack().Len() - 2).Content += context.Interpreter.GetOutputBufferStack().GetLast().Content
+	}
+
+	nativeFn_ob_clean(args, context)
 	return values.NewBool(true), nil
 }
 
@@ -146,8 +142,6 @@ func nativeFn_ob_get_clean(args []values.RuntimeValue, context runtime.Context) 
 	// This function calls the output handler (with the PHP_OUTPUT_HANDLER_CLEAN and PHP_OUTPUT_HANDLER_FINAL flags),
 	// discards it's return value, returns the contents of the active output buffer and turns off the active output buffer.
 
-	// TODO Throw notice if no buffer: e.g. Notice: ob_get_clean(): Failed to delete buffer. No buffer to delete in /home/user/scripts/code.php on line 5
-
 	if context.Interpreter.GetOutputBufferStack().Len() == 0 {
 		return values.NewBool(false), nil
 	}
@@ -155,6 +149,23 @@ func nativeFn_ob_get_clean(args []values.RuntimeValue, context runtime.Context) 
 	content := context.Interpreter.GetOutputBufferStack().GetLast().Content
 	context.Interpreter.GetOutputBufferStack().Pop()
 	return values.NewStr(content), nil
+}
+
+// -------------------------------------- ob_get_contents -------------------------------------- MARK: ob_get_contents
+
+func nativeFn_ob_get_contents(args []values.RuntimeValue, context runtime.Context) (values.RuntimeValue, phpError.Error) {
+	// Spec: https://www.php.net/manual/en/function.ob-get-contents.php
+
+	_, err := funcParamValidator.NewValidator("ob_get_contents").Validate(args)
+	if err != nil {
+		return values.NewVoid(), err
+	}
+
+	if context.Interpreter.GetOutputBufferStack().Len() == 0 {
+		return values.NewBool(false), nil
+	}
+
+	return values.NewStr(context.Interpreter.GetOutputBufferStack().GetLast().Content), nil
 }
 
 // -------------------------------------- ob_get_flush -------------------------------------- MARK: ob_get_flush
@@ -172,32 +183,14 @@ func nativeFn_ob_get_flush(args []values.RuntimeValue, context runtime.Context) 
 	// This function calls the output handler (with the PHP_OUTPUT_HANDLER_FINAL flag),
 	// flushes (sends) it's return value, returns the contents of the active output buffer and turns off the active output buffer.
 
-	// TODO Throw notice if no buffer: e.g. Notice: ob_get_flush(): Failed to delete buffer. No buffer to delete in /home/user/scripts/code.php on line 5
-
 	if context.Interpreter.GetOutputBufferStack().Len() == 0 {
+		context.Interpreter.PrintError(phpError.NewNotice("ob_get_flush(): Failed to delete buffer. No buffer to delete in %s", context.Stmt.GetPosition().ToPosString()))
 		return values.NewBool(false), nil
 	}
 
 	content := context.Interpreter.GetOutputBufferStack().GetLast().Content
 	nativeFn_ob_end_flush(args, context)
 	return values.NewStr(content), nil
-}
-
-// -------------------------------------- ob_get_contents -------------------------------------- MARK: ob_get_contents
-
-func nativeFn_ob_get_contents(args []values.RuntimeValue, context runtime.Context) (values.RuntimeValue, phpError.Error) {
-	// Spec: https://www.php.net/manual/en/function.ob-get-contents.php
-
-	_, err := funcParamValidator.NewValidator("nativeFn_ob_get_contents").Validate(args)
-	if err != nil {
-		return values.NewVoid(), err
-	}
-
-	if context.Interpreter.GetOutputBufferStack().Len() == 0 {
-		return values.NewBool(false), nil
-	}
-
-	return values.NewStr(context.Interpreter.GetOutputBufferStack().GetLast().Content), nil
 }
 
 // -------------------------------------- ob_get_level -------------------------------------- MARK: ob_get_level
