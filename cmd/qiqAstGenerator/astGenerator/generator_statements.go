3666b3e7510652bdddc3fda3ba4522e131291bb1
MasterZydra <github@davidhein.de>
2025-08-23 17:44:00 +0200

    astGenerator: Replaced `context any` with `_ any`

--- cmd/qiqAstGenerator/astGenerator/generator_statements.go
+++ cmd/qiqAstGenerator/astGenerator/generator_statements.go
@@ -5,12 +5,12 @@ import (
 )
 
 // ProcessBreakStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessBreakStmt(stmt *ast.BreakStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessBreakStmt(stmt *ast.BreakStatement, _ any) (any, error) {
 	panic("ProcessBreakStmt is unimplemented")
 }
 
 // ProcessClassDeclarationStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessClassDeclarationStmt(stmt *ast.ClassDeclarationStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessClassDeclarationStmt(stmt *ast.ClassDeclarationStatement, _ any) (any, error) {
 	variableName := toGoVarName(stmt.Name)
 	// Create new class declaration stmt
 	generator.println(`%s := ast.NewClassDeclarationStmt(0, nil, "%s", %s, %s)`, variableName, stmt.Name, toBoolStr(stmt.IsAbstract), toBoolStr(stmt.IsFinal))
@@ -20,67 +20,67 @@ func (generator *AstGenerator) ProcessClassDeclarationStmt(stmt *ast.ClassDeclar
 }
 
 // ProcessCompoundStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessCompoundStmt(stmt *ast.CompoundStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessCompoundStmt(stmt *ast.CompoundStatement, _ any) (any, error) {
 	panic("ProcessCompoundStmt is unimplemented")
 }
 
 // ProcessConstDeclarationStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessConstDeclarationStmt(stmt *ast.ConstDeclarationStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessConstDeclarationStmt(stmt *ast.ConstDeclarationStatement, _ any) (any, error) {
 	panic("ProcessConstDeclarationStmt is unimplemented")
 }
 
 // ProcessContinueStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessContinueStmt(stmt *ast.ContinueStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessContinueStmt(stmt *ast.ContinueStatement, _ any) (any, error) {
 	panic("ProcessContinueStmt is unimplemented")
 }
 
 // ProcessDeclareStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessDeclareStmt(stmt *ast.DeclareStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessDeclareStmt(stmt *ast.DeclareStatement, _ any) (any, error) {
 	panic("ProcessDeclareStmt is unimplemented")
 }
 
 // ProcessDoStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessDoStmt(stmt *ast.DoStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessDoStmt(stmt *ast.DoStatement, _ any) (any, error) {
 	panic("ProcessDoStmt is unimplemented")
 }
 
 // ProcessEchoStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessEchoStmt(stmt *ast.EchoStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessEchoStmt(stmt *ast.EchoStatement, _ any) (any, error) {
 	panic("ProcessEchoStmt is unimplemented")
 }
 
 // ProcessExpressionStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessExpressionStmt(stmt *ast.ExpressionStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessExpressionStmt(stmt *ast.ExpressionStatement, _ any) (any, error) {
 	panic("ProcessExpressionStmt is unimplemented")
 }
 
 // ProcessForStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessForStmt(stmt *ast.ForStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessForStmt(stmt *ast.ForStatement, _ any) (any, error) {
 	panic("ProcessForStmt is unimplemented")
 }
 
 // ProcessForeachStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessForeachStmt(stmt *ast.ForeachStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessForeachStmt(stmt *ast.ForeachStatement, _ any) (any, error) {
 	panic("ProcessForeachStmt is unimplemented")
 }
 
 // ProcessFunctionDefinitionStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessFunctionDefinitionStmt(stmt *ast.FunctionDefinitionStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessFunctionDefinitionStmt(stmt *ast.FunctionDefinitionStatement, _ any) (any, error) {
 	panic("ProcessFunctionDefinitionStmt is unimplemented")
 }
 
 // ProcessGlobalDeclarationStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessGlobalDeclarationStmt(stmt *ast.GlobalDeclarationStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessGlobalDeclarationStmt(stmt *ast.GlobalDeclarationStatement, _ any) (any, error) {
 	panic("ProcessGlobalDeclarationStmt is unimplemented")
 }
 
 // ProcessIfStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessIfStmt(stmt *ast.IfStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessIfStmt(stmt *ast.IfStatement, _ any) (any, error) {
 	panic("ProcessIfStmt is unimplemented")
 }
 
 // ProcessInterfaceDeclarationStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessInterfaceDeclarationStmt(stmt *ast.InterfaceDeclarationStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessInterfaceDeclarationStmt(stmt *ast.InterfaceDeclarationStatement, _ any) (any, error) {
 	variableName := toGoVarName(stmt.Name)
 	// Create new interface declaration stmt
 	generator.println(`%s := ast.NewInterfaceDeclarationStmt(0, nil, "%s")`, variableName, stmt.Name)
@@ -104,21 +104,21 @@ func (generator *AstGenerator) ProcessInterfaceDeclarationStmt(stmt *ast.Interfa
 }
 
 // ProcessReturnStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessReturnStmt(stmt *ast.ReturnStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessReturnStmt(stmt *ast.ReturnStatement, _ any) (any, error) {
 	panic("ProcessReturnStmt is unimplemented")
 }
 
 // ProcessStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessStmt(stmt *ast.Statement, context any) (any, error) {
+func (generator *AstGenerator) ProcessStmt(stmt *ast.Statement, _ any) (any, error) {
 	panic("ProcessStmt is unimplemented")
 }
 
 // ProcessThrowStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessThrowStmt(stmt *ast.ThrowStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessThrowStmt(stmt *ast.ThrowStatement, _ any) (any, error) {
 	panic("ProcessThrowStmt is unimplemented")
 }
 
 // ProcessWhileStmt implements ast.Visitor.
-func (generator *AstGenerator) ProcessWhileStmt(stmt *ast.WhileStatement, context any) (any, error) {
+func (generator *AstGenerator) ProcessWhileStmt(stmt *ast.WhileStatement, _ any) (any, error) {
 	panic("ProcessWhileStmt is unimplemented")
 }
