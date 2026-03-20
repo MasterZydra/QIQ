7985e6c11c9465aa2baacc7c7925b656c63e0820
MasterZydra <github@davidhein.de>
2026-03-20 23:15:23 +0100

    Runtime: Added default class `TypeError`

--- cmd/qiq/runtime/classes/classes.go
+++ cmd/qiq/runtime/classes/classes.go
@@ -67,6 +67,31 @@ func RegisterDefaultClasses(interpreter runtime.Interpreter) {
 
 	interpreter.AddClass(Error.Name, Error)
 
+	// -------------------------------------- TypeError -------------------------------------- MARK: TypeError
+
+	// Spec: https://www.php.net/manual/en/class.typeerror.php
+	TypeError := ast.NewClassDeclarationStmt(0, nil, "TypeError", false, false)
+	TypeError.BaseClass = "Error"
+	TypeError.AddProperty(ast.NewPropertyDeclarationStmt(0, nil, "$message", "protected", false, []string{"string"}, ast.NewStringLiteralExpr(0, nil, "", ast.DoubleQuotedString)))
+	TypeError.AddProperty(ast.NewPropertyDeclarationStmt(0, nil, "$string", "private", false, []string{"string"}, ast.NewStringLiteralExpr(0, nil, "", ast.DoubleQuotedString)))
+	TypeError.AddProperty(ast.NewPropertyDeclarationStmt(0, nil, "$code", "protected", false, []string{"int"}, nil))
+	TypeError.AddProperty(ast.NewPropertyDeclarationStmt(0, nil, "$file", "protected", false, []string{"string"}, ast.NewStringLiteralExpr(0, nil, "", ast.DoubleQuotedString)))
+	TypeError.AddProperty(ast.NewPropertyDeclarationStmt(0, nil, "$line", "protected", false, []string{"int"}, nil))
+	TypeError.AddProperty(ast.NewPropertyDeclarationStmt(0, nil, "$trace", "private", false, []string{"array"}, ast.NewArrayLiteralExpr(0, nil)))
+	TypeError.AddProperty(ast.NewPropertyDeclarationStmt(0, nil, "$previous", "private", false, []string{"null", "Throwable"}, ast.NewConstantAccessExpr(0, nil, "NULL")))
+	TypeError.AddMethod(ast.NewMethodDefinitionStmt(0, nil, "__construct", []string{"public"}, []ast.FunctionParameter{{Name: "$message", Type: []string{"string"}, DefaultValue: ast.NewStringLiteralExpr(0, nil, "", ast.DoubleQuotedString)}, {Name: "$code", Type: []string{"int"}, DefaultValue: ast.NewIntegerLiteralExpr(0, nil, 0)}, {Name: "$previous", Type: []string{"null", "Throwable"}, DefaultValue: ast.NewConstantAccessExpr(0, nil, "NULL")}}, ast.NewCompoundStmt(0, []ast.IStatement{ast.NewExpressionStmt(0, ast.NewSimpleAssignmentExpr(0, ast.NewMemberAccessExpr(0, nil, ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$this")), ast.NewConstantAccessExpr(0, nil, "message")), ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$message")))), ast.NewExpressionStmt(0, ast.NewSimpleAssignmentExpr(0, ast.NewMemberAccessExpr(0, nil, ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$this")), ast.NewConstantAccessExpr(0, nil, "code")), ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$code")))), ast.NewExpressionStmt(0, ast.NewSimpleAssignmentExpr(0, ast.NewMemberAccessExpr(0, nil, ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$this")), ast.NewConstantAccessExpr(0, nil, "previous")), ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$previous"))))}), []string{}))
+	TypeError.AddMethod(ast.NewMethodDefinitionStmt(0, nil, "getMessage", []string{"public", "final"}, []ast.FunctionParameter{}, ast.NewCompoundStmt(0, []ast.IStatement{ast.NewReturnStmt(0, nil, ast.NewMemberAccessExpr(0, nil, ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$this")), ast.NewConstantAccessExpr(0, nil, "message")))}), []string{"string"}))
+	TypeError.AddMethod(ast.NewMethodDefinitionStmt(0, nil, "getPrevious", []string{"public", "final"}, []ast.FunctionParameter{}, ast.NewCompoundStmt(0, []ast.IStatement{ast.NewReturnStmt(0, nil, ast.NewMemberAccessExpr(0, nil, ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$this")), ast.NewConstantAccessExpr(0, nil, "previous")))}), []string{"null", "Throwable"}))
+	TypeError.AddMethod(ast.NewMethodDefinitionStmt(0, nil, "getCode", []string{"public", "final"}, []ast.FunctionParameter{}, ast.NewCompoundStmt(0, []ast.IStatement{ast.NewReturnStmt(0, nil, ast.NewMemberAccessExpr(0, nil, ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$this")), ast.NewConstantAccessExpr(0, nil, "code")))}), []string{"int"}))
+	TypeError.AddMethod(ast.NewMethodDefinitionStmt(0, nil, "getFile", []string{"public", "final"}, []ast.FunctionParameter{}, ast.NewCompoundStmt(0, []ast.IStatement{ast.NewReturnStmt(0, nil, ast.NewMemberAccessExpr(0, nil, ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$this")), ast.NewConstantAccessExpr(0, nil, "file")))}), []string{"string"}))
+	TypeError.AddMethod(ast.NewMethodDefinitionStmt(0, nil, "getLine", []string{"public", "final"}, []ast.FunctionParameter{}, ast.NewCompoundStmt(0, []ast.IStatement{ast.NewReturnStmt(0, nil, ast.NewMemberAccessExpr(0, nil, ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$this")), ast.NewConstantAccessExpr(0, nil, "line")))}), []string{"int"}))
+	TypeError.AddMethod(ast.NewMethodDefinitionStmt(0, nil, "getTrace", []string{"public", "final"}, []ast.FunctionParameter{}, ast.NewCompoundStmt(0, []ast.IStatement{ast.NewReturnStmt(0, nil, ast.NewMemberAccessExpr(0, nil, ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$this")), ast.NewConstantAccessExpr(0, nil, "trace")))}), []string{"array"}))
+	TypeError.AddMethod(ast.NewMethodDefinitionStmt(0, nil, "getTraceAsString", []string{"public", "final"}, []ast.FunctionParameter{}, ast.NewCompoundStmt(0, []ast.IStatement{ast.NewReturnStmt(0, nil, ast.NewFunctionCallExpr(0, nil, ast.NewStringLiteralExpr(0, nil, "implode", ast.DoubleQuotedString), []ast.IExpression{ast.NewConstantAccessExpr(0, nil, "PHP_EOL"), ast.NewMemberAccessExpr(0, nil, ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$this")), ast.NewConstantAccessExpr(0, nil, "trace"))}))}), []string{"string"}))
+	TypeError.AddMethod(ast.NewMethodDefinitionStmt(0, nil, "__toString", []string{"public"}, []ast.FunctionParameter{}, ast.NewCompoundStmt(0, []ast.IStatement{ast.NewReturnStmt(0, nil, ast.NewMemberAccessExpr(0, nil, ast.NewSimpleVariableExpr(0, ast.NewVariableNameExpr(0, nil, "$this")), ast.NewConstantAccessExpr(0, nil, "string")))}), []string{"string"}))
+	TypeError.AddMethod(ast.NewMethodDefinitionStmt(0, nil, "__clone", []string{"private"}, []ast.FunctionParameter{}, ast.NewCompoundStmt(0, []ast.IStatement{}), []string{"void"}))
+
+	interpreter.AddClass(TypeError.Name, TypeError)
+
 	// -------------------------------------- ClosedGeneratorException -------------------------------------- MARK: ClosedGeneratorException
 
 	// Spec: https://www.php.net/manual/en/class.closedgeneratorexception.php
--- cmd/qiq/runtime/classes/classes.php
+++ cmd/qiq/runtime/classes/classes.php
@@ -110,6 +110,46 @@ class Error implements Throwable {
 // TODO CompileError
 // TODO ParseError
 // TODO TypeError
+
+// -------------------------------------- TypeError -------------------------------------- MARK: TypeError
+
+// Spec: https://www.php.net/manual/en/class.typeerror.php
+class TypeError extends Error {
+    /* Properties */
+    protected string $message = "";
+    private string $string = "";
+    protected int $code;
+    protected string $file = "";
+    protected int $line;
+    private array $trace = [];
+    private ?Throwable $previous = null;
+
+    /* Methods */
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null) {
+        $this->message = $message;
+        $this->code = $code;
+        $this->previous = $previous;
+    }
+
+    final public function getMessage(): string { return $this->message; }
+
+    final public function getPrevious(): ?Throwable { return $this->previous; }
+
+    final public function getCode(): int { return $this->code; }
+
+    final public function getFile(): string { return $this->file; }
+
+    final public function getLine(): int { return $this->line; }
+
+    final public function getTrace(): array { return $this->trace; }
+
+    final public function getTraceAsString(): string { return implode(PHP_EOL, $this->trace); }
+
+    public function __toString(): string { return $this->string; }
+
+    private function __clone(): void {}
+}
+
 // TODO ArgumentCountError
 // TODO ValueError
 // TODO ArithmeticError
